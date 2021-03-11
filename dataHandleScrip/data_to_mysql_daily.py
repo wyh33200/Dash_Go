d20ff1b7ccc647abf6f012818763ec708b4d661b
wyh33200 <18770038017@163.com>
2021-03-11 11:14:33 +0800

    定时任务

---
+++ dataHandleScrip/data_to_mysql_daily.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import time
+from sqlalchemy import create_engine
+from data_to_mysql import per_xxz_data_clean, data_to_mysql
+
+cnn_xxz_log = create_engine("mysql+mysqlconnector://xxzlog:xxz@log@192.168.2.6:3306/xxz_log")
+cnn_root = create_engine("mysql+mysqlconnector://root:root@localhost:3306/dash")
+
+
+def per_xxz_data_get(date_):
+    table_date = date_[:6]
+    table = 'per_wxapp_xxz_log_behavior_'+table_date
+    date_ = list(date_)
+    date_.insert(4, '-')
+    date_.insert(7, '-')
+    date_ = ''.join(date_)
+    sql = r"select distinct pid,DATE_FORMAT(logtime,'%Y-%m-%d') date,count(1) handle_num from " \
+          "{table} where logtime like '%{date_}%' group by 1,2".format(table=table, date_=date_)
+    print(sql)
+    data = pd.read_sql(sql, cnn_xxz_log)
+    print('获取{time}的数据'.format(time=date_))
+    return data
+
+
+# 增加定时任务
+if __name__ == '__main__':
+    while True:
+
+        if time.strftime('%H:%M') == '03:30':
+            time_now = time.strftime('%Y%m%d')
+            df = per_xxz_data_clean(per_xxz_data_get(time_now))
+            data_to_mysql(df)
+            print("{time}数据处理完成".format(time=time))
+            time.sleep(60)
