33a5e363d2323af0f1e85d22c0ac82a722310d0e
Cenpy <18770038017@163.com>
2021-04-23 17:07:25 +0800

    企业日报

--- dataHandleScrip/windows定时任务/company_report.py
+++ dataHandleScrip/windows定时任务/company_report.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../')
 from company_report_to_mysql_all import *
 
+
 if __name__ == '__main__':
     time_now = datetime.datetime.now() - datetime.timedelta(days=1)
     data = CompanyReport(time_now)
